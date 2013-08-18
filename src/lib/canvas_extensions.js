85125e617e0c561329ab64d878eb00df7a45da1c
Evan Hahn <me@evanhahn.com>
2013-08-18 10:18:27 -1000

    what if we could BLUR STUFF WITH SHADOWS COOL WOW

--- src/lib/canvas_extensions.js
+++ src/lib/canvas_extensions.js
@@ -2,6 +2,16 @@ extend(CanvasRenderingContext2D.prototype, {
 
 	outline: function(options) {
 
+		// do I need to blur
+		if (options.shadowBlur) {
+			this.shadowOffsetX = 0;
+			this.shadowOffsetY = 0;
+			this.shadowBlur = options.shadowBlur;
+			this.shadowColor = options.shadowColor;
+		} else {
+			this.shadowBlur = 0;
+		}
+
 		// do fill
 		if (options.fillColor) {
 			this.fillStyle = options.fillColor;
